9cf2bae6ef07e9d48f8d8baf57b0778de4f0d021
devflores84 <dev.flores84@gmail.com>
2022-11-04 14:48:42 -0600

    commit inicial
    Commit con el proyecto inicial de Ios para prueba CI - CD

---
+++ iosCiCdTest/iosCiCdTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosCiCdTestApp.swift
+//  iosCiCdTest
+//
+//  Created by MacBook on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iosCiCdTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
